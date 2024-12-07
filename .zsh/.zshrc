7afbfe85ba06c1b45ccf2fedd748b7098947e9a6
me-js-bro <me.js.bro@gmail.com>
2024-12-07 19:28:20 +0600

    updated so many things, added powerlevel10k theme

--- .zsh/.zshrc
+++ .zsh/.zshrc
@@ -1,7 +1,12 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
-source ~/.zsh/alias
-source ~/.zsh/functions
 
 # Show the runtime
 function preexec() {
@@ -18,11 +23,34 @@ function precmd() {
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="af-magic"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+eval "$(fzf --zsh)" # fzf
+eval "$(thefuck --alias)" # thefu*k
+eval "$(zoxide init zsh)"
+
+show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :1000 {}; fi"
+
+export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo \${}'"         "$@" ;;
+    ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+  esac
+}
 
 # case sensetivity
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-
 zstyle ':omz:update' mode reminder	# just remind me to update when it's time
 
 plugins=(
@@ -30,10 +58,13 @@ plugins=(
     zsh-autosuggestions
     jsontools
     web-search
-	  themes
     zsh-syntax-highlighting
     )
 
 source $ZSH/oh-my-zsh.sh
 
-neofetch || fastfetch
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+source ~/.zsh/alias
+source ~/.zsh/functions
--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#---------------#
+# ┏┳    ┳┓    
+#  ┃┏   ┣┫┏┓┏┓
+# ┗┛┛•  ┻┛┛ ┗┛     
+#---------------#
+
 # color defination
 red="\e[1;31m"
 green="\e[1;32m"
@@ -16,110 +22,82 @@ note="${megenta}[ NOTE ]${end}"
 done="${cyan}[ DONE ]${end}"
 error="${red}[ ERROR ]${end}"
 
-clear
+# log file
+dir=`pwd`
+log="$dir/zsh-install-$(date +%I:%M_%p).log"
+touch "$log"
 
-present_dir=`pwd`
-
-packages=(
+# required packages
+common_packages=(
     bat
     curl
+    eza
+    fastfetch
+    figlet
+    fzf
     git
-    lsd
-    zsh
+    xclip
+    zoxide
 )
 
-sleep 2
-
-# check which distro
-check_distro() {
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        
-        if [ "$ID" == "linuxmint" ] || [ "$NAME" == "Linux Mint" ]; then
-            printf "\e[38;5;47m󰣭\e[1;0m\n"
-        else
-            case "$ID" in
-                ubuntu)
-                    printf "${action} => Starting script for \e[38;5;208m\[e1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                debian)
-                    printf "${action} => Starting script for \e[38;5;89m\e[1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                zorin)
-                    printf "${action} => Starting script for \e[38;5;81m\e[1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                pop)
-                    printf "${action} => Starting script for \e[38;5;37m\e[1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                kali)
-                    printf "${action} => Starting script for \e[1;34\e[1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                parrot)
-                    printf "${action} => Starting script for \e[1;32m\e[1;0m\n"
-                    pkg_man="apt"
-                    ;;
-                arch)
-                    printf "${action} => Starting script for \e[1;36m󰣇\e[1;0m\n"
-                    pkg_man="pacman"
-                    ;;
-                manjaro)
-                    printf "${action} => Starting script for \e[38;5;47m\e[1;0m\n"
-                    pkg_man="pacman"
-                    ;;
-                arcolinux)
-                    printf "${action} => Starting script for \e[38;5;81m\e[1;0m\n"
-                    pkg_man="pacman"
-                    ;;
-                fedora)
-                    printf "${action} => Starting script for \e[38;5;33m\e[1;0m\n"
-                    pkg_man="dnf"
-                    ;;
-                opensuse*)
-                    printf "${action} => Starting script for \e[38;5;47m \e[1;0m\n"
-                    pkg_man="zypper"
-                    ;;
-                *)
-                    printf "${red}[ SORRY ]${end} => ${yellow}Your distro is not available right now. Very soon we will add your distro${end}... \n"
-                    ;;
-            esac
-        fi
-    else
-        printf "\n"
-    fi
-}
-
-check_distro && sleep 1
+for_opensuse=(
+    python311
+    python311-pip
+    python311-pipx
+    xclip
+)
 
 # package installation function
 fn_install() {
+    local pkg=$1
 
     if [ -n "$(command -v pacman)" ]; then  # Arch Linux
-        sudo pacman -S --noconfirm "$1"
+        sudo pacman -S --noconfirm "$pkg" 2>&1 | tee -a "$log"
     elif [ -n "$(command -v dnf)" ]; then  # Fedora
-        sudo dnf install -y "$1"
-    elif [ -n "$(command -v zypper)" ]; then  # openSUSE
-        sudo zypper in "$1"
-    elif [ -n "$(command -v apt)" ]; then	# ubuntu or ubuntu based
-    	sudo apt install "$1"
+        sudo dnf install -y "$pkg" 2>&1 | tee -a "$log"
+    elif [ -n "$(command -v zypper)" ]; then # opensuse
+        sudo zypper in -y "$pkg" 2>&1 | tee -a "$log"
+    elif [ -n "$(command -v apt)" ]; then	# debian
+        sudo apt install -y "$pkg" 2>&1 | tee -a "$log"
     else
         echo "Unsupported distribution."
         return 1
     fi
 }
 
-for pkgs in "${packages[@]}"; do
-   fn_install "$pkgs"
+# install the packages
+for pkgs in "${common_packages[@]}"; do
+   fn_install "$pkgs" 2>&1 | tee -a "$log"
 done
 
-clear
+if command -v zypper &> /dev/null; then
+    for pkgs in "${for_opensuse[@]}"; do
+        sudo zypper in -y "$pkgs" 2>&1 | tee -a "$log"
+    done
+
+    # installing thefu*k
+    if command -v pipx &> /dev/null; then
+        pipx runpip thefuck install setuptools &> /dev/null
+        sleep 0.5
+        pipx install --python python3.11 thefuck &> /dev/null 2>&1 | tee -a "$log"
+
+        if command -v thefuck &> /dev/null; then
+            printf "${done} - thef*ck was installed successfully!\n" && sleep 1
+        fi
+    fi
+
+elif command -v pacman &> /dev/null; then  # Arch Linux
+        sudo pacman -S --noconfirm thefuck 2>&1 | tee -a "$log"
+
+elif command -v dnf &> /dev/null; then  # Fedora
+        sudo dnf install -y thefuck 2>&1 | tee -a "$log"
+fi
+
+printf "${attention} - Installing bash files...\n \n \n" && sleep 0.5
+
 
 # ---- oh-my-zsh installation ---- #
-printf "${attention} - Now installing ${yellow} oh-my-zsh, zsh-autosuggestions, zsh-syntax-highlighting${end}...\n"
+printf "${attention}\n==> Now installing ${yellow} oh-my-zsh, zsh-autosuggestions, zsh-syntax-highlighting${end}...\n"
 sleep 2
 
 oh_my_zsh_dir="$HOME/.oh-my-zsh"
@@ -132,32 +110,34 @@ fi
  	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && \
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k \
 
 printf "${done} - Installation completed... Now changing default shell to ${cyan}zsh${end} \n"
-    chsh -s $(which zsh)
+chsh -s $(which zsh)
 sleep 1
 
 printf "${attention} - Now proceeding to the next step Configuring $HOME/.zshrc file\n"
 sleep 2
 
 # Check and backup the directories and file
-for item in "$HOME/.zsh" "$HOME/.zshrc" "$HOME/.config/lsd"; do
+mkdir -p "$HOME/.Zsh-Backup"-${USER}
+for item in "$HOME/.zsh" "$HOME/.zshrc" "$HOME/.p10k.zsh"; do
     if [[ -d $item ]]; then
         case $item in
             $HOME/.zsh)
                 printf "${note} - A ${green}.zsh${end} directory is available... Backing it up\n" 
-                mv "$item" "$HOME/.zsh"-${USER}
-                ;;
-            $HOME/.config/lsd)
-                printf "${note} - A ${yellow}~/.config/lsd${end} directory is available... Backing it up\n" 
-                mv "$item" "$HOME/.config/lsd"-${USEr}
+                mv "$item" "$HOME/.Zsh-Backup"-${USER}/
                 ;;
         esac
     elif [[ -f $item ]]; then
         case $item in
             $HOME/.zshrc)
                 printf "${note} - A ${cyan}.zshrc${end} file is available... Backing it up\n" 
-                mv "$item" "$HOME/.zshrc"-${USER}
+                mv "$item" "$HOME/.Zsh-Backup"-${USER}/
+                ;;
+            $HOME/.p10k.zsh)
+                printf "${note} - A ${cyan}.zshrc${end} file is available... Backing it up\n" 
+                mv "$item" "$HOME/.Zsh-Backup"-${USER}/
                 ;;
         esac
     fi
@@ -167,19 +147,16 @@ sleep 1
 
 printf "${action} - Copying configs\n"
 sleep 1
-cp -r .zsh ~/
+cp -r .zsh .p10k.zsh ~/
 ln -sf "$HOME/.zsh/.zshrc" "$HOME/.zshrc"
 
 printf "${done} - Installation and configuration of zsh and oh-my-zsh finished!\n"
-sleep 1 && clear
+sleep 1
 
 #___________________ Theme
 
-# Print the first part of the message
-printf "${green}[${end} Themes ${green}]${end} - ${yellow}"
-
 # Define the message for the typewriter effect
-message="If you want to change themes of your zsh prompt. Just follow these steps below..."
+message="Installation completed. Just close and open the terminal again."
 
 # Function to print with typewriter effect
 typewriter() {
@@ -193,13 +170,6 @@ typewriter() {
 }
 
 # Call the function with the message and a delay of 0.05 seconds
+clear
 typewriter " $message" 0.07
-
-sleep 0.5
-
-printf "\n\n${yellow}[ STEPS ]${end}\n" && sleep 0.7
-printf "${yellow}1.${end} Open your terminal and type ${yellow}theme${end}. It will generate oh-my-zsh themes randomly.\n" && sleep 0.5
-printf "${yellow}2.${end} Now copy the name of the theme you liked.\n" && sleep 0.5
-printf "${yellow}3.${end} Open ${yellow}$HOME/.zsh/.zshrc${end} file with your favourith text editor. and you will find ${yellow}ZSH_THEME=\"af-magic\"${end}.\n" && sleep 0.5
-printf "${yellow}4.${end} Change the ${yellow}\"af-magic\"${end} with the theme name you copied.\n" && sleep 0.5
-printf "${yellow}5.${end} Now type the command ${yellow}source ~/.zsh/.zshrc${end} and done....\n" && sleep 0.5
+exit 0
